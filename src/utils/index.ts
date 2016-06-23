0c17715152529f624a7e319c30056fdea1835cb6
Ben Teichman <ben.l.teichman@gmail.com>
2016-06-23 16:26:17 -0400

    fix url concatenation

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,6 +3,6 @@ import rawJsonp = require('jsonp');
 
 export function jsonp(url: string, body: any): Promise<any> {
   return new Promise((resolve, reject) => {
-    rawJsonp(`${this.url}?${qs.stringify(body)}`, (err, data) => err ? reject(err) : resolve(data));
+    rawJsonp(`${url}?${qs.stringify(body)}`, (err, data) => err ? reject(err) : resolve(data));
   });
 }
